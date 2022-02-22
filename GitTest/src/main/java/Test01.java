8bb0a921c6e2f98b918f800a45dc7474a81de838
tkroahdla@naver.com <tkroahdla@naver.com>
2022-02-22 16:19:25 +0900

    커밋용

--- GitTest/src/main/java/Test01.java
+++ GitTest/src/main/java/Test01.java
@@ -2,10 +2,8 @@
 public class Test01 {
 
 	public static void main(String[] args) {
-		
 		System.out.println("1¹øÂ° Ä¿¹Ô!!");
 		System.out.println("2¹øÂÅ Ä¿¹Ô!!");
-		System.out.println("¼¼Á¤ Ä¿¹ÔÇÔ!!");
 	}
 
 }
