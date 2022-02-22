dcfe4277e35990b1c9c10d839b7f584242b86cd7
smhrd <smhrd@222.102.104.180>
2022-02-22 16:58:32 +0900

    수진악마

--- GitTest/src/main/java/Test01.java
+++ GitTest/src/main/java/Test01.java
@@ -5,7 +5,7 @@ public class Test01 {
 		System.out.println("2¹øÂÅ Ä¿¹Ô!!");
 		System.out.println("¼¼Á¤ Ä¿¹Ô!!!!!!!!!!!!!!!!!");
 		System.out.println("¼ºÁØ Ä¿¹Ô!!");
-		System.out.println("¼öÁøÂ¯ ÃÖ°í~!!");
+		System.out.println("¼öÁøÂ¯ ÃÖ¾Ç¾Ç¾Ç¾Ç~!!");
 		System.out.println("½ÂÇöÀÌ ´õ ÃÖ°í~!!");
 		System.out.println("½ÂÇöÀÌ ´õ ÃÖ°í~2!!");
 		System.out.println("µ¿µ¿´í½º Çª½¬ È®ÀÎ ¿Ï·á!!");
