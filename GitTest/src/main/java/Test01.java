37fb48350c4aea0dee4f58440d37f9b8bb79072e
smhrd <smhrd@220.80.165.12>
2022-02-22 16:52:06 +0900

    화긴

--- GitTest/src/main/java/Test01.java
+++ GitTest/src/main/java/Test01.java
@@ -3,7 +3,7 @@ public class Test01 {
 	public static void main(String[] args) {
 		System.out.println("1¹øÂ° Ä¿¹Ô!!");
 		System.out.println("2¹øÂÅ Ä¿¹Ô!!");
-		System.out.println("¼¼Á¤ Ä¿¹Ô!!");
+		System.out.println("¼¼Á¤ Ä¿¹Ô!!!!!!!!!!!!!!!!!");
 		System.out.println("¼ºÁØ Ä¿¹Ô!!");
 		System.out.println("¼öÁøÂ¯ ÃÖ°í~!!");
 		System.out.println("½ÂÇöÀÌ ´õ ÃÖ°í~!!");
