2338996503d06e943354060e840264ee80a0a838
smhrd <smhrd@59.0.129.219>
2022-02-26 10:58:49 +0900

    add Employee dao, con

--- GitTest/src/main/java/com/model/EmployeeDAO.java
+++ GitTest/src/main/java/com/model/EmployeeDAO.java
@@ -116,4 +116,147 @@ public class EmployeeDAO {
 		return al;
 
 	}
+
+	public EmployeeVO selectOne(String id) {
+		EmployeeVO vo = null;
+	
+		try {
+			connect();
+			
+			String sql = "select emp_id, emp_name, emp_seat_no, emp_phone, emp_status, emp_joindate, emp_super_id, admin_yn, rfid_uid, area_id, plug_seq from t_employee where emp_id = ?";
+			psmt = conn.prepareStatement(sql);
+			rs = psmt.executeQuery();
+			psmt.setString(1, id);
+			
+			if(rs.next()){
+				String getid = rs.getString(1);
+				String getname = rs.getString(2);
+				String getseat = rs.getString(3);
+				String getphone = rs.getString(4);
+				String getstatus = rs.getString(5);
+				String getjoindate = rs.getString(6);
+				String getsuperid = rs.getString(7);
+				String getadminyn = rs.getString(8);
+				String getrfid = rs.getString(9);
+				String getarea = rs.getString(10);
+				int getplug = rs.getInt(11);
+				
+				vo = new EmployeeVO(getid, getname, getseat, getphone, getstatus, getjoindate, getsuperid, getadminyn, getrfid, getarea, getplug);
+			}		
+						
+		}catch(Exception e){
+			e.printStackTrace();
+		}finally {
+			close();
+		}
+		return vo;
+
+	}
+		
+	
+	// 직원 생성 시, 기본 pw는 id와 동일하게 설정(사용자가 직접 pw 수정하게끔)
+	// 직원 생성 시, 상태 값은 D(디폴트)로 설정
+	public int regEmp(String id, String name, String seat, String phone, String superid, String yn, String rfid, String area, int plug) {
+		int cnt = 0;
+		
+		try {
+			connect();
+			
+			String sql = "insert into t_employee values (?, ?, ?, ?, ?, D, sysdate, ?, ?, ?, ?, ?)";
+			psmt = conn.prepareStatement(sql);
+			psmt.setString(1, id);
+			psmt.setString(2, id);
+			psmt.setString(3, name);
+			psmt.setString(4, seat);
+			psmt.setString(5, phone);
+			psmt.setString(6, superid);
+			psmt.setString(7, yn);
+			psmt.setString(8, rfid);
+			psmt.setString(9, area);
+			psmt.setInt(10, plug);
+			
+			cnt = psmt.executeUpdate();
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		}finally {
+			close();
+		}
+		return cnt;
+	}
+	
+	//관리자용 직원정보수정
+	public int updateEmp1(String id, String name, String seat, String phone, String superid, String yn, String rfid, String area, int plug) {
+		int cnt = 0;
+		
+		try {
+			connect();
+			
+			String sql = "update t_employee set emp_name = ?, emp_seat_no = ?, emp_phone = ?, emp_super_id = ?, admin_yn = ?, rfid_uid = ?, plug_seq = ? where emp_id = ?";
+			psmt = conn.prepareStatement(sql);
+			psmt.setString(1, name);
+			psmt.setString(2, seat);
+			psmt.setString(3, phone);
+			psmt.setString(4, superid);
+			psmt.setString(5, yn);
+			psmt.setString(6, rfid);
+			psmt.setString(7, area);
+			psmt.setInt(8, plug);
+			psmt.setString(9, id);
+			
+			cnt = psmt.executeUpdate();
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		}finally {
+			close();
+		}
+		return cnt;
+	}
+	
+	//직원용 직원정보수정
+	public int updateEmp2(String id, String pw, String phone) {
+			int cnt = 0;
+			
+			try {
+				connect();
+				
+				String sql = "update t_employee set emp_pw = ?, emp_phone = ? where emp_id = ?";
+				psmt = conn.prepareStatement(sql);
+				psmt.setString(1, pw);
+				psmt.setString(2, phone);
+				psmt.setString(3, id);
+				
+				cnt = psmt.executeUpdate();
+				
+			}catch(Exception e){
+				e.printStackTrace();
+			}finally {
+				close();
+			}
+			return cnt;
+		}
+	
+	// 직원 정보 삭제
+	public int deleteEmp(String id) {
+		int cnt = 0;
+		
+		try {
+			connect();
+			
+			String sql = "delete from t_employee where emp_id = ?";
+			psmt = conn.prepareStatement(sql);
+			psmt.setString(1, id);
+			
+			cnt = psmt.executeUpdate();
+					
+		}catch(Exception e){
+			e.printStackTrace();
+		}finally {
+			close();
+		}
+		return cnt;
+	}
 }
+
+
