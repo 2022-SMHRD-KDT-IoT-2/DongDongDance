49b156439d620556e05cad7069166db731205b66
tkroa <tkroa@DESKTOP-C34468V>
2022-02-26 21:06:28 +0900

    Command 패키지 추가.

---
+++ GitTest/src/main/java/com/Command/Command.java
@@ -0,0 +1,12 @@
+package com.Command;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public interface Command {
+	
+	public abstract String execute(HttpServletRequest request, HttpServletResponse response);
+
+	
+	
+}
