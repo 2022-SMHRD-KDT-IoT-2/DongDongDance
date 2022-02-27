d63e4dbba9b73f6c1884c4f3dff3b4f23eeab25b
smhrd <smhrd@59.0.129.219>
2022-02-27 21:46:16 +0900

    update frontcontroller

--- GitTest/src/main/java/com/FrontController/FrontController.java
+++ GitTest/src/main/java/com/FrontController/FrontController.java
@@ -8,7 +8,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.Command.Command;
+import com.POJO.DeleteAreaCon;
+import com.POJO.DeleteEmpCon;
 import com.POJO.LoginCon;
+import com.POJO.LogoutCon;
+import com.POJO.RegAreaCon;
+import com.POJO.RegEmpCon;
+import com.POJO.UpdateEmp1Con;
+import com.POJO.UpdateEmp2Con;
 
 
 // 확장자패턴 사용 : 어떤 요청이던 뒤에 .do가 붙어 있으면 이 서블릿에서 받아서 처리
@@ -31,16 +38,20 @@ public class FrontController extends HttpServlet {
 		
 		if(result.equals("LoginCon.do")) {
 			command = new LoginCon();
-		}else if(result.equals("JoinCon.do")) {
-			
-		}else if(result.equals("UpdateCon.do")) {
-			
-		}else if(result.equals("DeleteCon.do")) {
-
 		}else if(result.equals("LogoutCon.do")) {
-			
-		}else if(result.equals("IdCheckCon.do")) {
-			
+			command = new LogoutCon();
+		}else if(result.equals("RegEmpCon.do")) {
+			command = new RegEmpCon();
+		}else if(result.equals("RegAreaCon.do")) {
+			command = new RegAreaCon();
+		}else if(result.equals("UpdateEmp1Con.do")) {
+			command = new UpdateEmp1Con();
+		}else if(result.equals("UpdateEmp2Con.do")) {
+			command = new UpdateEmp2Con();
+		}else if(result.equals("DeleteEmpCon.do")) {
+			command = new DeleteEmpCon();
+		}else if(result.equals("DeleteAreaCon.do")) {
+			command = new DeleteAreaCon();
 		}
 		
 		String url = command.execute(request, response);
