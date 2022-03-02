14e39c94ceb6e8660e61202c5538c9c4c4510ba9
tkroahdla@naver.com <tkroahdla@naver.com>
2022-03-02 16:43:09 +0900

    RfidDAO - uid로 emp_id찾는 메소드 추가

--- GitTest/src/main/java/com/DAO/RfidDAO.java
+++ GitTest/src/main/java/com/DAO/RfidDAO.java
@@ -5,6 +5,8 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
+import com.VO.EmployeeVO;
+
 public class RfidDAO {
 	Connection conn = null;
 	PreparedStatement psmt = null;
@@ -12,10 +14,10 @@ public class RfidDAO {
 
 	public void connect() {
 		try {
-            Class.forName("oracle.jdbc.driver.OracleDriver");
+			Class.forName("oracle.jdbc.driver.OracleDriver");
 			String url = "jdbc:oracle:thin:@project-db-stu.ddns.net";
 			String dbid = "campus_d_3_0216";
-			String dbpw = "smhrd3";			
+			String dbpw = "smhrd3";
 			conn = DriverManager.getConnection(url, dbid, dbpw);
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -37,50 +39,50 @@ public class RfidDAO {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public int regLog(String type, String id) {
 		int cnt = 0;
-		
+
 		try {
 			connect();
-			
+
 			String sql = "insert into t_rfid_log values (t_rfid_log_seq.nextval, ?, sysdate, ?, null)";
 			psmt = conn.prepareStatement(sql);
 			psmt.setString(1, type);
 			psmt.setString(2, id);
 
 			cnt = psmt.executeUpdate();
-			
-		}catch(Exception e){
+
+		} catch (Exception e) {
 			e.printStackTrace();
-		}finally {
+		} finally {
 			close();
 		}
 		return cnt;
 	}
-	
+
 	public int updateLog(int seq, String type, String memo) {
 		int cnt = 0;
-		
+
 		try {
 			connect();
-			
+
 			String sql = "update t_rfid_log set log_type = ?, emp_id = ? where log_seq = ?";
 			psmt = conn.prepareStatement(sql);
 			psmt.setString(1, type);
 			psmt.setString(2, memo);
 			psmt.setInt(3, seq);
-			
+
 			cnt = psmt.executeUpdate();
-			
-		}catch(Exception e){
+
+		} catch (Exception e) {
 			e.printStackTrace();
-		}finally {
+		} finally {
 			close();
 		}
 		return cnt;
 	}
-	
+
 	public int deleteLog(int seq) {
 		int cnt = 0;
 		try {
@@ -99,7 +101,27 @@ public class RfidDAO {
 		}
 		return cnt;
 	}
-	
-	
-	
+
+	public String select_empid(String uid) {
+
+		try {
+			connect();
+			String sql = "select emp_id from t_employee where rfid_uid = ?";
+			psmt = conn.prepareStatement(sql);
+			psmt.setString(1, uid);
+			rs = psmt.executeQuery();
+
+			if (rs.next()) {
+				String getid = rs.getString(1);
+				return getid;
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			close();
+		}
+		return null;
+	}
+
 }
