9a985800db2adfc1f9a9128c930770c6eeddb146
tkroa <tkroa@DESKTOP-C34468V>
2022-03-06 13:13:59 +0900

    RFID READ test

--- GitTest/src/main/java/rfid/Rfid_read.java
+++ GitTest/src/main/java/rfid/Rfid_read.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.DAO.EmployeeDAO;
 import com.DAO.RfidDAO;
 
 @WebServlet("/Rfid_read")
@@ -22,12 +23,34 @@ public class Rfid_read extends HttpServlet {
 		out.println(uid);
 		
 //		String uid = "1234";
-		RfidDAO dao = new RfidDAO();
+		RfidDAO R_dao = new RfidDAO();
+		EmployeeDAO E_dao = new EmployeeDAO();
 		
-		String uid2 = dao.select_empid(uid);
+		String empid = R_dao.select_empid(uid);
 		System.out.println(uid2);
 		
-		dao.regLog("1", uid2);
+		R_dao.regLog("1", empid);
+		
+		if(empid!=null) {
+//			E_dao.updateStatus(uid, "1");
+			System.out.println("해당사원이없어요.");
+		}
+		//1. uid가 찍히면 read하고 직원 status값이 무엇인지 가져와야함.
+		//1-1. 그렇다면 찍혔을 때 확인하고 갱신해야함.
+		//2. 가져온 status값에 따라 동작이 달라짐.
+		
+		String status =E_dao.select_status(uid);
+		System.out.println("상태값 : " + status);
+		
+		if(status.equals("0") ) {
+			E_dao.updateStatus(uid,"1");
+			System.out.println("상태값이 0");
+		}
+		else {
+			E_dao.updateStatus(uid,"0");
+			System.out.println("상태값이 1");
+		}
+	
 	}
 
 }
