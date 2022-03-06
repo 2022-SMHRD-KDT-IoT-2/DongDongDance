d62024578c061a8f427efe24c300c4a89dbde6cf
tkroa <tkroa@DESKTOP-C34468V>
2022-03-06 13:25:20 +0900

    rfid test1

--- GitTest/src/main/java/rfid/Rfid_read.java
+++ GitTest/src/main/java/rfid/Rfid_read.java
@@ -16,41 +16,42 @@ import com.DAO.RfidDAO;
 public class Rfid_read extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String uid = request.getParameter("uid");
-		System.out.println(uid);
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String uid = request.getParameter("uid"); // 아두이노에서 uid값을 받아온다.
 		PrintWriter out = response.getWriter();
-		out.println(uid);
-		
+		System.out.println(uid); // 출력확인
+		out.println(uid); // 출력확인
+
 //		String uid = "1234";
 		RfidDAO R_dao = new RfidDAO();
 		EmployeeDAO E_dao = new EmployeeDAO();
-		
+
 		String empid = R_dao.select_empid(uid);
-		System.out.println(uid2);
-		
-		R_dao.regLog("1", empid);
-		
-		if(empid!=null) {
+		System.out.println(empid);
+		R_dao.regLog("1", empid); // (LOG_TYPE = 1) 로 RFID로그 - 테이블에 삽입
+
+		if (empid == null) {
 //			E_dao.updateStatus(uid, "1");
 			System.out.println("해당사원이없어요.");
 		}
-		//1. uid가 찍히면 read하고 직원 status값이 무엇인지 가져와야함.
-		//1-1. 그렇다면 찍혔을 때 확인하고 갱신해야함.
-		//2. 가져온 status값에 따라 동작이 달라짐.
-		
-		String status =E_dao.select_status(uid);
-		System.out.println("상태값 : " + status);
-		
-		if(status.equals("0") ) {
-			E_dao.updateStatus(uid,"1");
-			System.out.println("상태값이 0");
-		}
+		// 1. uid가 찍히면 read하고 직원 status값이 무엇인지 가져와야함.
+		// 1-1. 그렇다면 찍혔을 때 확인하고 갱신해야함.
+		// 2. 가져온 status값에 따라 동작이 달라짐.
+
 		else {
-			E_dao.updateStatus(uid,"0");
-			System.out.println("상태값이 1");
+			String status = E_dao.select_status(uid);
+			System.out.println("상태값 : " + status);
+			
+			if (status.equals("0")) {
+				E_dao.updateStatus(uid, "1");
+				System.out.println("상태값이 0");
+			} else {
+				E_dao.updateStatus(uid, "0");
+				System.out.println("상태값이 1");
+			}
 		}
-	
+
 	}
 
 }
