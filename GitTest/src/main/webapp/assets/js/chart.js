160fca97d384887ab50fb57954804910c67410bb
smhrd <smhrd@59.0.234.149>
2022-03-07 15:19:53 +0900

    전력량측정누적

--- GitTest/src/main/webapp/assets/js/chart.js
+++ GitTest/src/main/webapp/assets/js/chart.js
@@ -6,6 +6,13 @@ function addData(chart, label, data) {
     chart.update();
 }
 
+function changeLastData(chart, data) {
+	let count = chart.data.datasets[0].data.length; 
+	chart.data.datasets[0].data[count - 1] = data;    
+	chart.update();
+}
+
+
 function removeData(chart) {
     chart.data.labels.pop();
     chart.data.datasets.forEach((dataset) => {
