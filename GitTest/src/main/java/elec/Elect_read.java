68e45cf58bbafe54d02ff56bad95e11822a695e3
tkroa <tkroa@DESKTOP-C34468V>
2022-03-09 20:33:26 +0900

    전류값 4개 받아오는 서블릿 작성, 실시간 DB삽입 완료 -동진-

--- GitTest/src/main/java/elec/Elect_read.java
+++ GitTest/src/main/java/elec/Elect_read.java
@@ -13,20 +13,41 @@ import com.DAO.PlugSenDAO;
 
 @WebServlet("/Elect_read")
 public class Elect_read extends HttpServlet {
-
-	private static final long serialVersionUID = 1L;
+	
+	public double doub(String str) {
+		double value1f = Double.parseDouble(str);
+		if(value1f == 0.09 || value1f == -0.09) {
+			value1f = 0;
+		}
+		return value1f;
+	}
 
 	protected void service(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		
-		String uid = request.getParameter("uid");
-		System.out.println(uid);
-		PrintWriter out = response.getWriter();
-		out.println(uid);
+		String value1 = request.getParameter("value1");
+		String value2 = request.getParameter("value2");
+		String value3 = request.getParameter("value3");
+		String value4 = request.getParameter("value4");
 		
-		float elect = 12.34f;
+		System.out.println("value1 :"+ value1);
+		System.out.println("value2 :"+ value2);
+		System.out.println("value3 :"+ value3);
+		System.out.println("value4 :"+ value4);
+		System.out.println();
 		PlugSenDAO dao = new PlugSenDAO();
-		dao.regPlugSen(3,elect);
+		
+		if(value1 != null) {
+			double value1f = doub(value1);
+			double value2f = doub(value2);
+			double value3f = doub(value3);
+			double value4f = doub(value4);
+			
+			dao.regPlugSen(3,value1f);
+			dao.regPlugSen(45,value2f);
+			dao.regPlugSen(47,value3f);
+			dao.regPlugSen(49,value4f);
+		}
 		
 	}
 
